eee6e6f5be2e0d69b36b457b2560fe117f9df0de
David Endrych <david.endrych@gmail.com>
2018-09-22 12:38:40 +0200

    Pridani tabulek hra,vydavatel,herni zanr

--- API/config/iis-db.sql
+++ API/config/iis-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Sob 22. zář 2018, 11:24
+-- Vytvořeno: Sob 22. zář 2018, 12:37
 -- Verze serveru: 10.1.34-MariaDB
 -- Verze PHP: 7.2.7
 
@@ -21,6 +21,55 @@ SET time_zone = "+00:00";
 --
 -- Databáze: `iis-db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `game`
+--
+
+CREATE TABLE `game` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) COLLATE utf16_czech_ci NOT NULL,
+  `ReleaseDate` date DEFAULT NULL,
+  `Description` text COLLATE utf16_czech_ci,
+  `PublisherId` int(11) DEFAULT NULL,
+  `Image` tinytext COLLATE utf16_czech_ci NOT NULL,
+  `Video` tinytext COLLATE utf16_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `game_genre`
+--
+
+CREATE TABLE `game_genre` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(55) COLLATE utf16_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `game_genre_games`
+--
+
+CREATE TABLE `game_genre_games` (
+  `GameId` int(11) NOT NULL,
+  `GameGenreId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_czech_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) COLLATE utf16_czech_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_czech_ci;
 
 -- --------------------------------------------------------
 
@@ -50,10 +99,75 @@ INSERT INTO `user` (`Nickname`, `Firstname`, `Lastname`, `Email`, `Phone`, `Pass
 --
 
 --
+-- Klíče pro tabulku `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `PublisherId` (`PublisherId`);
+
+--
+-- Klíče pro tabulku `game_genre`
+--
+ALTER TABLE `game_genre`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Klíče pro tabulku `game_genre_games`
+--
+ALTER TABLE `game_genre_games`
+  ADD PRIMARY KEY (`GameId`,`GameGenreId`),
+  ADD KEY `GameGenreId` (`GameGenreId`);
+
+--
+-- Klíče pro tabulku `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- Klíče pro tabulku `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`Nickname`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `game`
+--
+ALTER TABLE `game`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pro tabulku `game_genre`
+--
+ALTER TABLE `game_genre`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pro tabulku `publisher`
+--
+ALTER TABLE `publisher`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `game`
+--
+ALTER TABLE `game`
+  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`PublisherId`) REFERENCES `publisher` (`Id`),
+  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`Id`) REFERENCES `game_genre_games` (`GameId`);
+
+--
+-- Omezení pro tabulku `game_genre_games`
+--
+ALTER TABLE `game_genre_games`
+  ADD CONSTRAINT `game_genre_games_ibfk_1` FOREIGN KEY (`GameGenreId`) REFERENCES `game_genre` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
