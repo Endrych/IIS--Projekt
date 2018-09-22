97197bd6570a1b47193df74f30b6ea4ecf5a0f4d
kukucka <kukin1996@seznam.cz>
2018-09-22 23:52:57 +0200

    DOdelan zakladni layout ,pridan zakladni styling, vyreseny transformace obrazku

--- SPA/src/components/LandingPage/FavoriteGames.js
+++ SPA/src/components/LandingPage/FavoriteGames.js
@@ -15,57 +15,82 @@ import "../../styles/LandingPage/favoriteGames.css";
 class FavoriteGames extends Component {
 	render() {
 		return (
-
-			<Col xs="12" sm="12">
-				<div style={{ display: "flex" }}>
-					<div className="favorite__col-1 ">
-						<NavLink to="/games/leagueoflegends">
-							<img alt="LoL" src={LoL} className="default-user favorite__picture--type-1"  style={{backgroundColor:"red" }}/>
-						</NavLink>
-					</div>
-					<div className="favorite__col-2">
-						<div className="favorite__col--item">
-							<NavLink to="/games/overwatch">
-								<img alt="Overwatch" src={Overwatch} className="favorite__picture--type-2 default-user" />
-							</NavLink>
+			<Col className="favorite__game-wrapper" xs="12" sm="12">
+				<Row>
+					<Col xs="12" sm="12">
+						<div className="favorite__title">Nejpopulárnější hry</div>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs="12" sm="12">
+						<div style={{ display: "flex" }}>
+							<div className="favorite__col-1 ">
+								<NavLink to="/games/leagueoflegends">
+									<img
+										alt="LoL"
+										src={LoL}
+										className="default-user favorite__picture--type-1"
+										style={{ backgroundColor: "red" }}
+									/>
+								</NavLink>
+							</div>
+							<div className="favorite__col-2">
+								<div className="favorite__col--item">
+									<NavLink to="/games/overwatch">
+										<img
+											alt="Overwatch"
+											src={Overwatch}
+											className="favorite__picture--type-2 default-user"
+										/>
+									</NavLink>
+								</div>
+								<div className="favorite__col--item favorite__col--item-midle">
+									<NavLink to="/games/csgo">
+										<img
+											alt="CS:GO"
+											src={Csgo}
+											className="favorite__picture--type-2 favorite__picture--middle default-user"
+										/>
+									</NavLink>
+								</div>
+								<div className="favorite__col--item">
+									<NavLink to="/games/dota2">
+										<img
+											alt="Dota2"
+											src={Dota2}
+											className="favorite__picture--type-2 default-user"
+										/>
+									</NavLink>
+								</div>
+							</div>
+							<div className="favorite__col-3">
+								<div className="favorite__col--item">
+									<NavLink to="/games/pubg">
+										<img alt="PUBG" src={Pubg} className="favorite__picture--type-2 default-user" />
+									</NavLink>
+								</div>
+								<div className="favorite__col--item favorite__col--item-midle">
+									<NavLink to="/games/hearthstone">
+										<img
+											alt="HearthStone"
+											src={HearthStone}
+											className="favorite__picture--type-2 favorite__picture--middle default-user"
+										/>
+									</NavLink>
+								</div>
+								<div className="favorite__col--item">
+									<NavLink to="/games/fortnite">
+										<img
+											alt="Fortnite"
+											src={Fortnite}
+											className="favorite__picture--type-2 default-user"
+										/>
+									</NavLink>
+								</div>
+							</div>
 						</div>
-						<div className="favorite__col--item favorite__col--item-midle">
-							<NavLink to="/games/csgo">
-								<img
-									alt="CS:GO"
-									src={Csgo}
-									className="favorite__picture--type-2 favorite__picture--middle default-user"
-								/>
-							</NavLink>
-						</div>
-						<div className="favorite__col--item">
-							<NavLink to="/games/dota2">
-								<img alt="Dota2" src={Dota2} className="favorite__picture--type-2 default-user" />
-							</NavLink>
-						</div>
-					</div>
-					<div className="favorite__col-3">
-						<div className="favorite__col--item">
-							<NavLink to="/games/pubg">
-								<img alt="PUBG" src={Pubg} className="favorite__picture--type-2 default-user" />
-							</NavLink>
-						</div>
-						<div className="favorite__col--item favorite__col--item-midle">
-							<NavLink to="/games/hearthstone">
-								<img
-									alt="HearthStone"
-									src={HearthStone}
-									className="favorite__picture--type-2 favorite__picture--middle default-user"
-								/>
-							</NavLink>
-						</div>
-						<div className="favorite__col--item">
-							<NavLink to="/games/fortnite">
-								<img alt="Fortnite" src={Fortnite} className="favorite__picture--type-2 default-user" />
-							</NavLink>
-						</div>
-					</div>
-				</div>
+					</Col>
+				</Row>
 			</Col>
 		);
 	}
