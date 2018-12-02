df57da05a860d535def97e1a0aff7550781f187f
kukucka <kukin1996@seznam.cz>
2018-12-02 19:20:47 +0100

    Oprava pridavani a odebirani prav

--- SPA2/src/components/Admin/admin_grant_rights.js
+++ SPA2/src/components/Admin/admin_grant_rights.js
@@ -17,31 +17,32 @@ class AdminGrantRights extends Component{
 		let toRender = "";
 		const { managePlayers } = field;
 		const { grantRights } = field;
-		if(!grantRights.waiting){
+		console.log(managePlayers.try, managePlayers.type, ">SAD>ASD>S>AD")
+		if(!grantRights.waiting || managePlayers.try){
 			if(grantRights.status === 403){
-				hasError = "Práva tohoto uživatele nelze manipulovat!";
+				hasError = <div className="text-danger">Práva tohoto uživatele nelze manipulovat!</div>;
 			}else if(grantRights.status === 204){
-				hasError = "Vámi zadaný uživatel neexistuje!";
+				hasError = <div className="text-danger">Vámi zadaný uživatel neexistuje!</div>;
 			}else{
 				if(managePlayers.try){
 					switch(managePlayers.type){
 						case "GRANT_SUCESS":
-							toRender = <div className="success">Práva úspěšně přidána</div>;
+							toRender = <div className="text-success">Práva úspěšně přidána</div>;
 							break;
 						case "GRANT_FAILED":
-							toRender = <div className="error">Práva se nezdařilo přidat</div>;
+							toRender = <div className="text-danger">Práva se nezdařilo přidat</div>;
 							break;
 						case "REMOVE_SUCESS":
-							toRender = <div className="success">Práva úspěšně odebrána</div>;
+							toRender = <div className="text-success">Práva úspěšně odebrána</div>;
 							break;
 						case "REMOVE_FAILED":
-							toRender = <div className="error">Práva se nezdařilo odebrat</div>;
+							toRender = <div className="text-danger">Práva se nezdařilo odebrat</div>;
 							break;
 						case "DEACTIVATE_SUCESS":
-							toRender = <div className="success">Deaktivace proběhla úspěšně</div>;
+							toRender = <div className="text-success">Deaktivace proběhla úspěšně</div>;
 							break;
 						case "DEACTIVATE_FAILED":
-							toRender = <div className="error">Deaktivace se nezdařila</div>;
+							toRender = <div className="text-danger">Deaktivace se nezdařila</div>;
 							break;
 					}
 				}
@@ -50,7 +51,7 @@ class AdminGrantRights extends Component{
 
 		return (
 			<div className={className}>
-				<label>{field.label}</label>
+				<label><b>{field.label}</b></label>
 				<input className="form-control" {...field.input}  />
 				{toRender}
 				{hasError}
@@ -79,14 +80,22 @@ class AdminGrantRights extends Component{
 		const {handleSubmit, managePlayers} = this.props;
 
 		return(
-			<div>
-				<form>
-					<Field name="Nickname" label="Přezdívka" managePlayers={managePlayers} props={this.props} component={this.renderInputField} />
-					<button onClick={handleSubmit(this.onSubmit.bind(this, "grant"))} className="btn btn-primary">Přidat práva</button>
-					<button onClick={handleSubmit(this.onSubmit.bind(this, "remove"))} className="btn btn-primary">Odebrat práva</button>
-					<button onClick={handleSubmit(this.onSubmit.bind(this, "deactivate"))} className="btn btn-danger">Deaktivovat účet</button>
-				</form>
-				<Link to="/user"><button className="btn btn-primary">Zpět</button></Link>
+			<div className="row row__box">
+				<div className="col col-sm-12">
+					<form>
+						<div className="col col-sm-12">
+							<Field name="Nickname" label="Přezdívka" managePlayers={managePlayers} props={this.props} component={this.renderInputField} />
+						</div>
+						<div className="col col-sm-12">
+							<button style={{marginRight: "5px"}} onClick={handleSubmit(this.onSubmit.bind(this, "grant"))} className="btn btn-primary">Přidat práva</button>
+							<button style={{marginRight: "5px"}} onClick={handleSubmit(this.onSubmit.bind(this, "remove"))} className="btn btn-primary">Odebrat práva</button>
+							<button onClick={handleSubmit(this.onSubmit.bind(this, "deactivate"))} className="btn btn-danger">Deaktivovat účet</button>
+						</div>
+					</form>
+					<div className="col col-sm-12">
+						<Link to="/user"><button  style={{marginTop: "5px"}} className="btn btn-primary">Zpět</button></Link>
+					</div>
+				</div>
 			</div>
 		)
 	}
