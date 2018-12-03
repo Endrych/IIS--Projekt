add4e01bf5be1f85b8c2c764301fcd8dcd413f42
David Endrych <david.endrych@gmail.com>
2018-12-03 19:08:06 +0100

    Add log

--- API/helpers/processError.js
+++ API/helpers/processError.js
@@ -1,6 +1,9 @@
+const fs = require('fs')
+
 module.exports = (res, error) =>{
     if (error.error) {
         console.log(error.error);
+        fs.appendFile('log.txt',JSON.stringify(error.error,null,4));
     }
     res.sendStatus(error.status);
 }
